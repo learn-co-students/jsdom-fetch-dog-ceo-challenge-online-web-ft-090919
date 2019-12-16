14180e2ff47776742c8bc58a281d6afa952d9a34
Doug Kintop <dkintop@hotmail.com>
2019-12-16 16:00:44 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,1 @@
 console.log("%c HI", "color: firebrick");
-
-function loadImages() {
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-  fetch(imgUrl)
-    .then(res => res.json())
-    .then(results => {
-      results.message.forEach(image => renderImage(image));
-    });
-}
-
-renderImage(image){
-    let img = document.createElement(img);
-    img.src = image
-    let div = document.querySelector('div#dog-image-container')
-    div.appendChild(img)
-}
-
-addEventListener("DOMContentLoaded", loadImages());
