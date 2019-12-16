a491b761a6aff9f59971f1afea5616593f5c14be
Doug Kintop <dkintop@hotmail.com>
2019-12-16 15:56:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 console.log("%c HI", "color: firebrick");
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-
-function fetchPics() {
-  fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(resp => resp.json())
-    .then(json => console.log(json));
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetch(imgUrl)
+    .then(res => res.json())
+    .then(results => {
+      results.message.forEach(image => renderImage(image));
+    });
 }
 
-function renderPics(json) {
-  json.forEach(pic => {
-    let img = document.createElement("img");
-    img.src = pic;
-  });
+renderImage(image){
+    let img = document.createElement(img);
+    img.src = image
+    let div = document.querySelector('div#dog-image-container')
+    div.appendChild(img)
 }
 
-addEventListener("DOMContentLoaded", fetchPics());
+addEventListener("DOMContentLoaded", loadImages());
