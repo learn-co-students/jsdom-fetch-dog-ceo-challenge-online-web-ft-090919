29d2d00a36d0456ffce348f553dbd58c08b1122f
Doug Kintop <dkintop@hotmail.com>
2019-12-16 15:30:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+function fetchPics() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => console.log(json));
+}
+
+function renderPics(json) {
+  json.forEach(pic => {
+    let img = document.createElement("img");
+    img.src = pic;
+  });
+}
+
+addEventListener("DOMContentLoaded", fetchPics());
